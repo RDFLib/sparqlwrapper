9fb464b5054015a781b740dcfb9d35b049c9f22f
Sergio Fernández <sergio@wikier.org>
2015-11-03 14:15:01 +0100

    released 1.7.2 with a hotfix for issue #63

--- SPARQLWrapper/__init__.py
+++ SPARQLWrapper/__init__.py
@@ -163,7 +163,7 @@ The package was greatly inspired by U{Lee Feigenbaum's similar package for Javas
 @requires: U{RDFLib<http://rdflib.net>} package.
 """
 
-__version__ = "1.7.2.dev0"
+__version__ = "1.7.2"
 """The version of SPARQLWrapper"""
 
 __authors__  = "Ivan Herman, Sergio Fernández, Carlos Tejo Alonso, Alexey Zakhlestin"
