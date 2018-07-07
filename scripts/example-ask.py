34a0c1adcfad5bab2ff521fd22d40d94bb43de0b
Carlos Tejo <carlos.tejo@landportal.info>
2018-07-07 19:39:00 +0200

    Added ASK example

---
+++ scripts/example-ask.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from SPARQLWrapper import SPARQLWrapper, JSON, XML, CSV, TSV
+
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql.setQuery("""
+    ASK WHERE { 
+        <http://dbpedia.org/resource/Asturias> rdfs:label "Asturias"@es
+    }    
+""")
+
+# JSON example
+print '\n\n*** JSON Example'
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+print results
+
+# XML example
+print '\n\n*** XML Example'
+sparql.setReturnFormat(XML)
+results = sparql.query().convert()
+print results.toxml()
+
+# CSV example
+print '\n\n*** CSV Example'
+sparql.setReturnFormat(CSV)
+results = sparql.query().convert()
+print results
+
+# TSV example
+print '\n\n*** TSV Example'
+sparql.setReturnFormat(TSV)
+results = sparql.query().convert()
+print results
