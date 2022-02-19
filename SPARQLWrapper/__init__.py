c9319f6e45a324ab61e68ab57d5ebe1b8c255b5a
eggplants <w10776e8w@yahoo.co.jp>
2022-02-20 04:43:02 +0900

    rm: useless info from init

--- SPARQLWrapper/__init__.py
+++ SPARQLWrapper/__init__.py
@@ -12,21 +12,6 @@ format.
 __version__ = "1.9.0.dev0"
 """The version of SPARQLWrapper"""
 
-__authors__ = "Ivan Herman, Sergio Fernández, Carlos Tejo Alonso, Alexey Zakhlestin"
-"""The primary authors of SPARQLWrapper"""
-
-__license__ = "W3C® SOFTWARE NOTICE AND LICENSE, http://www.w3.org/Consortium/Legal/copyright-software"
-"""The license governing the use and distribution of SPARQLWrapper"""
-
-__url__ = "http://rdflib.github.io/sparqlwrapper"
-"""The URL for SPARQLWrapper's homepage"""
-
-__contact__ = "rdflib-dev@googlegroups.com"
-"""Mail list to contact to other people RDFLib and SPARQLWrappers folks and developers"""
-
-__date__ = "2019-04-18"
-"""Last update"""
-
 __agent__ = "sparqlwrapper %s (rdflib.github.io/sparqlwrapper)" % __version__
 
 
@@ -36,5 +21,6 @@ from .Wrapper import GET, POST
 from .Wrapper import SELECT, CONSTRUCT, ASK, DESCRIBE, INSERT, DELETE
 from .Wrapper import URLENCODED, POSTDIRECTLY
 from .Wrapper import BASIC, DIGEST
+
 from .SmartWrapper import SPARQLWrapper2
 from .sparql_dataframe import get_sparql_dataframe
